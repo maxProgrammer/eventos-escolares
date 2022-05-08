eb02ee2c4b7d765f311eec16f0f4711fab61caef
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-08 20:48:46 -0300

    configurado lombok e swagger ui

--- src/main/java/com/api/eventos/controllers/EventoController.java
+++ src/main/java/com/api/eventos/controllers/EventoController.java
@@ -4,6 +4,9 @@ import com.api.eventos.dtos.EventoDto;
 import com.api.eventos.models.EventoModel;
 import com.api.eventos.services.EventoService;
 import com.fasterxml.jackson.databind.util.BeanUtil;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +22,8 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/evento")
+@RequestMapping("/event")
+@OpenAPIDefinition(info = @Info(title = "Event API", version = "1.0", description = "REST API responsible to allow events CRUD."))
 public class EventoController {
     final EventoService eventoService;
 
@@ -28,6 +32,7 @@ public class EventoController {
     }
 
     @PostMapping
+    @Operation(summary = "Add an event")
     public ResponseEntity<Object> saveEvento(@RequestBody @Valid EventoDto eventoDto) {
         var eventoModel = new EventoModel();
         //convert DTO em Model
@@ -36,11 +41,13 @@ public class EventoController {
     }
 
     @GetMapping
+    @Operation(summary = "Return event list")
     public ResponseEntity<Page<EventoModel>> getAllEventos(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(eventoService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Return event byID")
     public ResponseEntity<Object> getOneEvento(@PathVariable(value= "id") UUID id){
         Optional<EventoModel> eventoModelOptional = eventoService.findById(id);
         if(!eventoModelOptional.isPresent()){
@@ -50,6 +57,7 @@ public class EventoController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update event byID")
     public ResponseEntity<Object> updateEvento(@PathVariable(value = "id") UUID id, @RequestBody @Valid EventoDto eventoDto){
         Optional<EventoModel> eventoModelOptional = eventoService.findById(id);
         if(!eventoModelOptional.isPresent()){
@@ -64,6 +72,7 @@ public class EventoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete event byID")
     public ResponseEntity<Object> deleteEvento(@PathVariable(value = "id") UUID id){
         Optional<EventoModel> eventoModelOptional = eventoService.findById(id);
         if(!eventoModelOptional.isPresent()){
